84d6ea31fdb4135c82438e8c7c68319c2c06fc12
cgathergood <calumgathergood@gmail.com>
2018-05-27 21:34:10 +1000

    Android Studio Project created

---
+++ augmented_images/app/src/main/java/com/google/ar/core/examples/java/augmentedimage/AugmentedImageActivity.java
@@ -0,0 +1,238 @@
+package com.google.ar.core.examples.java.augmentedimage;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.google.ar.core.ArCoreApk;
+import com.google.ar.core.AugmentedImage;
+import com.google.ar.core.AugmentedImageDatabase;
+import com.google.ar.core.Config;
+import com.google.ar.core.Frame;
+import com.google.ar.core.Session;
+import com.google.ar.core.examples.java.augmentedimage.sceneform.AugmentedImageNode;
+
+import com.google.ar.core.examples.java.common.helpers.CameraPermissionHelper;
+import com.google.ar.core.examples.java.common.helpers.FullScreenHelper;
+import com.google.ar.core.examples.java.common.helpers.SnackbarHelper;
+import com.google.ar.core.exceptions.CameraNotAvailableException;
+import com.google.ar.core.exceptions.UnavailableApkTooOldException;
+import com.google.ar.core.exceptions.UnavailableArcoreNotInstalledException;
+import com.google.ar.core.exceptions.UnavailableSdkTooOldException;
+import com.google.ar.core.exceptions.UnavailableUserDeclinedInstallationException;
+import com.google.ar.sceneform.ArSceneView;
+import com.google.ar.sceneform.FrameTime;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+
+public class AugmentedImageActivity extends AppCompatActivity {
+    private static final String TAG = AugmentedImageActivity.class.getSimpleName();
+
+    // Rendering. The Renderers are created here, and initialized when the GL surface is created.
+    private ArSceneView arSceneView;
+    private ImageView fitToScanView;
+
+    private boolean installRequested;
+
+    private Session session;
+    private final SnackbarHelper messageSnackbarHelper = new SnackbarHelper();
+
+    private boolean shouldConfigureSession = false;
+
+    // Augmented image configuration and rendering.
+    private final Map<AugmentedImage, AugmentedImageNode> augmentedImageMap = new HashMap<>();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        arSceneView = findViewById(R.id.surfaceview);
+        fitToScanView = findViewById(R.id.image_view_fit_to_scan);
+
+        installRequested = false;
+
+        initializeSceneView();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        if (session == null) {
+            Exception exception = null;
+            String message = null;
+            try {
+                switch (ArCoreApk.getInstance().requestInstall(this, !installRequested)) {
+                    case INSTALL_REQUESTED:
+                        installRequested = true;
+                        return;
+                    case INSTALLED:
+                        break;
+                }
+
+                // ARCore requires camera permissions to operate. If we did not yet obtain runtime
+                // permission on Android M and above, now is a good time to ask the user for it.
+                if (!CameraPermissionHelper.hasCameraPermission(this)) {
+                    CameraPermissionHelper.requestCameraPermission(this);
+                    return;
+                }
+
+                session = new Session(/* context = */ this);
+            } catch (UnavailableArcoreNotInstalledException
+                    | UnavailableUserDeclinedInstallationException e) {
+                message = "Please install ARCore";
+                exception = e;
+            } catch (UnavailableApkTooOldException e) {
+                message = "Please update ARCore";
+                exception = e;
+            } catch (UnavailableSdkTooOldException e) {
+                message = "Please update this app";
+                exception = e;
+            } catch (Exception e) {
+                message = "This device does not support AR";
+                exception = e;
+            }
+
+            if (message != null) {
+                messageSnackbarHelper.showError(this, message);
+                Log.e(TAG, "Exception creating session", exception);
+                return;
+            }
+
+            shouldConfigureSession = true;
+        }
+
+        if (shouldConfigureSession) {
+            configureSession();
+            shouldConfigureSession = false;
+            arSceneView.setupSession(session);
+        }
+
+        // Note that order matters - see the note in onPause(), the reverse applies here.
+        try {
+            session.resume();
+            arSceneView.resume();
+        } catch (CameraNotAvailableException e) {
+            // In some cases (such as another camera app launching) the camera may be given to
+            // a different app instead. Handle this properly by showing a message and recreate the
+            // session at the next iteration.
+            messageSnackbarHelper.showError(this, "Camera not available. Please restart the app.");
+            session = null;
+            return;
+        }
+        fitToScanView.setVisibility(View.VISIBLE);
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        if (session != null) {
+            // Note that the order matters - GLSurfaceView is paused first so that it does not try
+            // to query the session. If Session is paused before GLSurfaceView, GLSurfaceView may
+            // still call session.update() and get a SessionPausedException.
+            arSceneView.pause();
+            session.pause();
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] results) {
+        if (!CameraPermissionHelper.hasCameraPermission(this)) {
+            Toast.makeText(
+                    this, "Camera permissions are needed to run this application", Toast.LENGTH_LONG)
+                    .show();
+            if (!CameraPermissionHelper.shouldShowRequestPermissionRationale(this)) {
+                // Permission denied with checking "Do not ask again".
+                CameraPermissionHelper.launchPermissionSettings(this);
+            }
+            finish();
+        }
+    }
+
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus) {
+        super.onWindowFocusChanged(hasFocus);
+        FullScreenHelper.setFullScreenOnWindowFocusChanged(this, hasFocus);
+    }
+
+    private void initializeSceneView() {
+        arSceneView.getScene().setOnUpdateListener((this::onUpdateFrame));
+    }
+
+    private void onUpdateFrame(FrameTime frameTime) {
+        Frame frame = arSceneView.getArFrame();
+        Collection<AugmentedImage> updatedAugmentedImages =
+                frame.getUpdatedTrackables(AugmentedImage.class);
+        for (AugmentedImage augmentedImage : updatedAugmentedImages) {
+            switch (augmentedImage.getTrackingState()) {
+                case PAUSED:
+                    // When an image is in PAUSED state, but the camera is not PAUSED, it has been detected,
+                    // but not yet tracked.
+                    break;
+
+                case TRACKING:
+                    // Have to switch to UI Thread to update View.
+                    fitToScanView.setVisibility(View.GONE);
+
+                    // Create a new anchor for newly found images.
+                    if (!augmentedImageMap.containsKey(augmentedImage)) {
+                        AugmentedImageNode node = new AugmentedImageNode(this);
+                        node.setImage(augmentedImage);
+                        augmentedImageMap.put(augmentedImage, node);
+                        arSceneView.getScene().addChild(node);
+                    }
+                    break;
+
+                case STOPPED:
+                    augmentedImageMap.remove(augmentedImage);
+                    break;
+
+                default:
+                    break;
+            }
+        }
+    }
+
+    private void configureSession() {
+        Config config = new Config(session);
+        if (!setupAugmentedImageDatabase(config)) {
+            messageSnackbarHelper.showError(this, "Could not setup augmented image database");
+        }
+        config.setUpdateMode(Config.UpdateMode.LATEST_CAMERA_IMAGE);
+        session.configure(config);
+    }
+
+    private boolean setupAugmentedImageDatabase(Config config) {
+        AugmentedImageDatabase augmentedImageDatabase;
+
+        Bitmap augmentedImageBitmap = loadAugmentedImageBitmap();
+        if (augmentedImageBitmap == null) {
+            return false;
+        }
+
+        augmentedImageDatabase = new AugmentedImageDatabase(session);
+        augmentedImageDatabase.addImage("targetImage", augmentedImageBitmap);
+
+        config.setAugmentedImageDatabase(augmentedImageDatabase);
+        return true;
+    }
+
+    private Bitmap loadAugmentedImageBitmap() {
+        try (InputStream is = getAssets().open("default.jpg")) {
+            return BitmapFactory.decodeStream(is);
+        } catch (IOException e) {
+            Log.e(TAG, "IO exception loading augmented image bitmap.", e);
+        }
+        return null;
+    }
+}
---
+++ augmented_images/app/src/main/java/com/google/ar/core/examples/java/augmentedimage/sceneform/AugmentedImageNode.java
@@ -0,0 +1,68 @@
+
+package com.google.ar.core.examples.java.augmentedimage.sceneform;
+
+import android.content.Context;
+import android.net.Uri;
+import android.util.Log;
+
+import com.google.ar.core.AugmentedImage;
+import com.google.ar.core.Pose;
+import com.google.ar.sceneform.AnchorNode;
+import com.google.ar.sceneform.Node;
+import com.google.ar.sceneform.math.Quaternion;
+import com.google.ar.sceneform.math.Vector3;
+import com.google.ar.sceneform.rendering.ModelRenderable;
+
+import java.util.concurrent.CompletableFuture;
+
+public class AugmentedImageNode extends AnchorNode {
+
+    private static final String TAG = "AugmentedImageNode";
+
+    private AugmentedImage image;
+    private static CompletableFuture<ModelRenderable> modelFuture;
+
+    public AugmentedImageNode(Context context) {
+        // Upon construction, start loading the modelFuture
+        if (modelFuture == null) {
+            modelFuture = ModelRenderable.builder().setRegistryId("modelFuture")
+                    .setSource(context, Uri.parse("NOVELO_EARTH.sfb"))
+                    .build();
+        }
+    }
+
+    /**
+     * Called when the AugmentedImage is detected and should be rendered. A Sceneform node tree is
+     * created based on an Anchor created from the image.
+     *
+     * @param image
+     */
+    public void setImage(AugmentedImage image) {
+        this.image = image;
+
+        if (!modelFuture.isDone()) {
+            CompletableFuture.allOf(modelFuture).thenAccept((Void aVoid) -> {
+                setImage(image);
+            }).exceptionally(throwable -> {
+                Log.e(TAG, "Exception loading", throwable);
+                return null;
+            });
+        }
+
+        setAnchor(image.createAnchor(image.getCenterPose()));
+
+        Node node = new Node();
+
+        Pose pose = Pose.makeTranslation(0.0f, 0.0f, 0.0f);
+
+        node.setParent(this);
+        node.setLocalPosition(new Vector3(pose.tx(), pose.ty(), pose.tz()));
+        node.setLocalRotation(new Quaternion(pose.qx(), pose.qy(), pose.qz(), pose.qw()));
+        node.setRenderable(modelFuture.getNow(null));
+
+    }
+
+    public AugmentedImage getImage() {
+        return image;
+    }
+}
